b66ea796b487b578783be8c0b7fad084f168fa3f
Ruben de Bruin <34062862+RubendeBruin@users.noreply.github.com>
2024-03-18 13:45:59 +0100

    RAOs ook erbij

--- src/wavedave/plots/wavespectrum.py
+++ src/wavedave/plots/wavespectrum.py
@@ -5,25 +5,23 @@ import matplotlib.pyplot as plt
 from waveresponse import DirectionalSpectrum
 
 
-def plot_wavespectrum(ds : DirectionalSpectrum, cmap = 'RdPu', above_title = '', do_periods = True, max_period = 15, ax=None, levels = None):
-
+def _plot_polar(ds: DirectionalSpectrum, ax, do_periods, cmap, max_period, levels):
     freq, dirs, vals = ds.grid(freq_hz=True, degrees=True)
 
     if do_periods:
-        y_value = 1/freq     # seconds
+        y_value = 1 / freq  # seconds
     else:
         y_value = freq
 
     # close the spectrum by repeating the first direction at the end
-    dirs = np.append(dirs, dirs[0]+360)
-
+    dirs = np.append(dirs, dirs[0] + 360)
 
     # Convert angles from degrees to radians and adjust so that 0 is at the top
     angles = np.radians(dirs)
 
     #
     # # add the vals as well
-    vals = np.hstack((vals, vals[:,0:1]))
+    vals = np.hstack((vals, vals[:, 0:1]))
 
     # Create a meshgrid for period and angles
     r, theta = np.meshgrid(y_value, angles)
@@ -33,9 +31,6 @@ def plot_wavespectrum(ds : DirectionalSpectrum, cmap = 'RdPu', above_title = '',
     if ax is None:
         fig = plt.figure(figsize=(6, 6))
         ax = fig.add_subplot(111, polar=True)
-
-    ax.set_theta_zero_location('N')  # Set 0 degrees to the top
-    ax.set_theta_direction(-1)  # Set angle direction to clockwise
 
     if do_periods:
         if max_period is not None:
@@ -47,16 +42,46 @@ def plot_wavespectrum(ds : DirectionalSpectrum, cmap = 'RdPu', above_title = '',
 
     ax.contourf(theta, r, vals.transpose(), cmap=cmap, levels=levels)
 
-    ax.set_title(f'{above_title}Hs = {ds.hs:.2f}m '
-                 f'Tp = {ds.tp:.1f}s'
-                 f' From = {ds.dirm(degrees=True):.0f}deg')
-
-    # radial labels
-    ax.set_rlabel_position(60)
-    label_position = ax.get_rlabel_position()
-    ax.text(np.radians(label_position - 10), 1.05*ax.get_rmax() , 'Period [s]',
-            ha='left', va='center', fontsize=10, font = 'Arial')
-
     return fig, ax
 
 
+def plot_wavespectrum(
+    ds: DirectionalSpectrum,
+    cmap="RdPu",
+    above_title="",
+    do_periods=True,
+    max_period=15,
+    ax=None,
+    levels=None,
+):
+
+    fig, ax = _plot_polar(ds, ax, do_periods, cmap, max_period, levels)
+
+    ax.set_theta_zero_location("N")  # Set 0 degrees to the top
+    ax.set_theta_direction(-1)  # Set angle direction to clockwise
+
+    ax.set_title(
+        f"{above_title}Hs = {ds.hs:.2f}m "
+        f"Tp = {ds.tp:.1f}s"
+        f" From = {ds.dirm(degrees=True):.0f}deg"
+    )
+
+    # radial labels
+    if do_periods:
+        r_text = "Period [s]"
+    else:
+        r_text = "Frequency [Hz]"
+
+    ax.set_rlabel_position(60)
+    label_position = ax.get_rlabel_position()
+    ax.text(
+        np.radians(label_position - 10),
+        1.05 * ax.get_rmax(),
+        r_text,
+        ha="left",
+        va="center",
+        fontsize=10,
+        font="Arial",
+    )
+
+    return fig, ax
--- src/wavedave/rao/rao.py
+++ src/wavedave/rao/rao.py
@@ -11,15 +11,16 @@ import pickle
 from pathlib import Path
 
 import waveresponse as wr
+from wavedave.plots.wavespectrum import _plot_polar
 
 
 class RAO(wr.RAO):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        description = "Boot 10"
-        mode = "heave"
-        response_unit = "m"   # unit of response , so without the /m for waves.
+        self.description = "Boot 10"
+        self.mode = "heave"
+        self.response_unit = "m"   # unit of response , so without the /m for waves.
 
     def save(self, filename):
         """Save the RAO to a file"""
@@ -30,6 +31,13 @@ class RAO(wr.RAO):
         """Load the RAO from a file (pickle format)"""
         return pickle.load(open(filename, "rb"))
 
+    def plot(self, ax=None):
+        """Plot the RAO"""
+        fig, ax = _plot_polar(self, ax, do_periods=True, cmap="RdPu", max_period=15, levels=None)
+
+        ax.set_title(self.description + f" {self.mode} RAO [{self.response_unit}/m]")
+
+        return fig, ax
 
 if __name__ == "__main__":
     # Example usage
@@ -51,7 +59,7 @@ if __name__ == "__main__":
     vals *= scale
 
     spread_fun = wr.CosineFullSpreading(s=2, degrees=True)
-    dirs = np.linspace(0.0, 180.0, num=16, endpoint=False)
+    dirs = np.linspace(0.0, 360.0, num=16, endpoint=False)
 
     wave = wr.WaveSpectrum.from_spectrum1d(
         freqs,
@@ -65,21 +73,48 @@ if __name__ == "__main__":
         waves_coming_from=False,
     )
 
-    rao =wr.RAO(freq_hz=True,
-           freq = wave.freq,
-           dirs = wave.dirs,
-           vals = wave.vals,)
+    freq, dirs, values = wave.grid(freq_hz=True,degrees=True)
 
-    rao.a
+    rao =wr.RAO(freq = freq,
+           dirs = dirs,
+           vals = values,
+                freq_hz=True,
+                degrees=True,)
+
 
     # plot
     #
     import matplotlib.pyplot as plt
     #
-    # plt.plot(freqs, vals)
+    # # plt.plot(freqs, vals)
+    # # plt.show()
+    # from wavedave.plots.wavespectrum import plot_wavespectrum
+    # plot_wavespectrum(wave)
     # plt.show()
-    from wavedave.plots.wavespectrum import plot_wavespectrum
-    plot_wavespectrum(wave)
-    plt.show()
 
-    # rao = RAO()
+    rao = RAO(freq = freq,
+           dirs = dirs,
+           vals = values,
+                freq_hz=True,
+                degrees=True,)
+
+    rao.description = "Boot 11"
+    rao.mode = "pitch"
+    rao.response_unit = "degrees"
+
+    # save
+    import tempfile
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        rao.save(tmpdirname + "/rao.pkl")
+
+
+        rao2 = RAO.load(tmpdirname + "/rao.pkl")
+        print(rao2.description)
+        print(rao2.mode)
+        print(rao2.response_unit)
+
+        # plot
+        rao2.plot()
+
+    plt.show()
